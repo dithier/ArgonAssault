b556fa9d13e92d0d6e64466bdd3d94079061f4c7
Carter Ithier <ithier@Carters-MacBook-Pro.local>
2019-09-02 21:05:58 -0400

    started basic score ui

--- ArgonAssault/Assets/Scripts/Enemy.cs
+++ ArgonAssault/Assets/Scripts/Enemy.cs
@@ -1,25 +1,27 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        Collider enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
+        enemyBoxCollider.isTrigger = false;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnParticleCollision(GameObject other)
     {
         print("Particles collided with enemy " + gameObject.name);
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        // set the parent of the transform to the parent provided
+        fx.transform.parent = parent;
         Destroy(this.gameObject);
-        print(gameObject.name + " destroyed");
     }
 }
