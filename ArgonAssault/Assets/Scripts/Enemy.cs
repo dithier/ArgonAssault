5c674b7e1a28ca7a093af258376fe6111dd1d81e
Carter Ithier <ithier@Carters-MacBook-Pro.local>
2019-09-03 10:21:16 -0400

    added scoreboard funcationality

--- ArgonAssault/Assets/Scripts/Enemy.cs
+++ ArgonAssault/Assets/Scripts/Enemy.cs
@@ -7,14 +7,21 @@ public class Enemy : MonoBehaviour
 {
     [SerializeField] GameObject deathFX;
     [SerializeField] Transform parent;
+    [SerializeField] int scorePerHit = 12;
+    ScoreBoard scoreBoard;
 
     // Start is called before the first frame update
     void Start()
     {
+        AddBoxCollider();
+        scoreBoard = FindObjectOfType<ScoreBoard>();
+    }
+
+    private void AddBoxCollider()
+    {
         Collider enemyBoxCollider = gameObject.AddComponent<BoxCollider>();
         enemyBoxCollider.isTrigger = false;
     }
-
 
     private void OnParticleCollision(GameObject other)
     {
@@ -23,5 +30,6 @@ public class Enemy : MonoBehaviour
         // set the parent of the transform to the parent provided
         fx.transform.parent = parent;
         Destroy(this.gameObject);
+        scoreBoard.ScoreHit(scorePerHit);
     }
 }
